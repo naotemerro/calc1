1d7c7f8347d3998e7ed9316b38d9e60a35d2c032
Victor Marques Ferreira <105400962+naotemerro@users.noreply.github.com>
2022-10-12 23:18:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//Tentativa de Calculadora
+
+
+using System;
+
+//Variaveis
+float n1 = 0; //Primeiro valor informado no console
+float n2 = 0; //Segundo valor informado no console
+string a = "";
+string s = "";
+string m = "";
+string d = "";
+
+Console.WriteLine("|____Calculadora____|"); 
+Console.WriteLine("|Primeiro valor:____|");
+n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("|Segundo valor:_____|");
+n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Qual Operação? _____|");
+Console.WriteLine("a - Adição!_______|");
+Console.WriteLine("s Subtração!______|");
+Console.WriteLine("m Multiplicação___|");
+Console.WriteLine("d Divisão!________|");
+
+if (a == a)
+{
+
+    a = Console.ReadLine();
+    Console.WriteLine("O resultado é: " + n1 + n2);
+
+    if (s == s)
+    s = Console.ReadLine();
+    Console.WriteLine("O resultado é: "); 
+    
+    m = Console.ReadLine();
+
+    d = Console.ReadLine();
+
+}
